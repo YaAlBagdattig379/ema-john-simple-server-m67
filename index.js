4d8c870c3d90eafa8489e64c4d6aab09215a100a
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-04 12:26:00 +0530

    m67 v8 side Effect are solved such as aftr clck pagination btn got error on Order summery

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port =  process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 
@@ -43,9 +43,17 @@ async function run(){
         // res.json(count); 
         res.send({count}); 
      })
-    }finally{
-    // 
-   }   
+    //  use to POST to get products by ids
+     app.post('/productByKeys',async(req,res)=>{
+          const keys = req.body; 
+          const ids = keys.map(id=> new ObjectId(id));
+          const query = {_id: {$in: ids}};
+          const cursor = productCollection.find(query);
+          const products = await cursor.toArray()
+          res.send(products)
+          console.log(keys)
+     })
+    }finally{}   
 
 }
 run().catch(console.dir);
